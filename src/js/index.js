e0599fa2fac00dcb6c0623e4d90a3a8597e38f85
Juan B <jjuanbr97@gmail.com>
2023-07-26 02:29:28 -0300

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,64 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([-28.237851, -52.393144], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
+L.marker([-28.237851, -52.393144]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
+
+    var popup = L.popup();
+
+
+const albtn = document.getElementById("aleatorio")
+const btns = document.getElementById("btn-search");
+const ipAddress = document.getElementById("search");
+
+const sip = document.getElementById("IPADD");
+const locations = document.getElementById("location");
+const timezone = document.getElementById("timezone");
+const isp = document.getElementById("ISP");
+
+
+
+albtn.addEventListener("click", () => {
+
+    const ipAleatoria = generarIpAleatoria();
+    ipAddress.value = ipAleatoria;
+    ip();
+  });
+
+
+btns.addEventListener("click", () => {
+    ip();
+  
+});
+
+async function ip() {
+    const ipp = ipAddress.value;
+
+  const url = await fetch(
+    `https://geo.ipify.org/api/v2/country?apiKey=at_diXvpgkmIad5743V3LV0vp3kbJAnU&ipAddress=${ipp}`
+  );
+  const json = await url.json();
+  console.log(json);
+  let ip = json.ip;
+  const loc = json.location.region
+  const tim = json.location.timezone
+  const issp = json.isp
+ 
+  sip.innerHTML = ip
+  locations.innerHTML = loc
+  timezone.innerHTML = tim
+  isp.innerHTML = issp
+
+}
+function generarIpAleatoria() {
+    const octeto1 = Math.floor(Math.random() * 256);
+    const octeto2 = Math.floor(Math.random() * 256);
+    const octeto3 = Math.floor(Math.random() * 256);
+    const octeto4 = Math.floor(Math.random() * 256);
+    const ipAleatoria = `${octeto1}.${octeto2}.${octeto3}.${octeto4}`;
+    return ipAleatoria;
+  }
